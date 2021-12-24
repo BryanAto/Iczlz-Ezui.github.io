bdb7b6aabb11e70d431c226a7c0f8a23b20d0c13
BryanAto <84436572+BryanAto@users.noreply.github.com>
2021-12-24 17:50:27 +0000

    Test-1

---
+++ script.js
@@ -0,0 +1,14 @@
+function TransitBuro() {
+  var Value = document.getElementById("Area-Bureau").value;
+
+  if(Value = "Kiriair_fan Galaxy Wolf Banana") {
+    document.getElementById("Area-Bureau").style.opacity = 1;
+  }
+}
+
+
+
+
+
+
+
